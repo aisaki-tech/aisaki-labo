9177db1f5fe0f23338e1720778930a1d1c5061d2
y-ishii-code <yuya.ishii.engineer@gmail.com>
2024-09-28 21:51:53 +0900

    fix

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,8 @@
-import eslintConfigPrettier from "eslint-config-prettier"; // Add
+import eslintConfigPrettier from 'eslint-config-prettier' // Add
 
 export default [
   {
-    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
   },
-  { languageOptions: { globals: globals.browser } },
   eslintConfigPrettier,
-];
+]
