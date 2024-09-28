358179ea0dcd03aa6c91ad481da439e5982cc234
y-ishii-code <yuya.ishii.engineer@gmail.com>
2024-09-28 21:51:45 +0900

    fix

--- public/js/test.js
+++ public/js/test.js
@@ -1,12 +1,9 @@
-
 const clickButtonEventListener = async () => {
-
-    const result = await fetch('/api/test')
-    console.log(await result.json())
+  const result = await fetch('/api/test')
+  console.log(await result.json())
 }
 
 window.addEventListener('load', () => {
-
-    const button = document.getElementById('button1');
-    button.addEventListener('click', clickButtonEventListener)
+  const button = document.getElementById('button1')
+  button.addEventListener('click', clickButtonEventListener)
 })
--- server/app.js
+++ server/app.js
@@ -1,28 +1,26 @@
-import express from "express";
-import path from "path";
-import { dirname } from "path";
-import { fileURLToPath } from "url";
+import express from 'express'
+import path from 'path'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
 
-app.get("/", (req, res) => {
-  res.send("Hello World３２!");
-});
+app.get('/', (req, res) => {
+  res.send('Hello World３２!')
+})
 
-app.get("/api/test", (req, res) => {
+app.get('/api/test', (req, res) => {
   const response = {
-    code: "0001",
-    message: "test!!!",
-  };
-  res.send(JSON.stringify(response));
-});
+    code: '0001',
+    message: 'test!!!',
+  }
+  res.send(JSON.stringify(response))
+})
 
 // public配下は静的ファイルを置ける
-app.use(
-  express.static(path.join(dirname(fileURLToPath(import.meta.url)), "public")),
-);
+app.use(express.static(path.join(dirname(fileURLToPath(import.meta.url)), 'public')))
